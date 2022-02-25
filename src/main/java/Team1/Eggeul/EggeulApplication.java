4bbb1cc938c5e8797bc26580605d081176ebd773
movmovmovmov <kej708@gmail.com>
2022-02-25 15:45:15 +0900

    테스트

---
+++ src/main/java/Team1/Eggeul/EggeulApplication.java
@@ -0,0 +1,13 @@
+package Team1.Eggeul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggeulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggeulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Team1/Eggeul/EggeulApplicationTests.java
@@ -0,0 +1,13 @@
+package Team1.Eggeul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggeulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
